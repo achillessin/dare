ba34f1fa0f23ba4ae73e03d0cc6adca380ea1652
sisil mehta <sisilmet2000@gmail.com>
2014-10-05 17:38:21 -0700

    Updated login mechanism to use parse

---
+++ src/com/dare/DareApplication.java
@@ -0,0 +1,25 @@
+package com.dare;
+
+import java.util.Arrays;
+import java.util.List;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseFacebookUtils;
+
+public class DareApplication  extends Application{
+	static final String TAG = "DareApplication";
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+
+		Parse.initialize(this, getString(R.string.ParseApplicationID),
+				getString(R.string.ParseClientID));
+
+		//TODO: add app permissions here
+		ParseFacebookUtils.initialize(getString(R.string.app_id));
+
+	}
+}
