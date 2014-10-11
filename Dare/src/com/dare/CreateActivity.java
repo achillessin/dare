750048acfec936555845abc108f2a29a5f15cf9a
Peter Cheng <chepeter@lab126.com>
2014-10-11 13:44:55 -0700

    added some challenge params

--- Dare/src/com/dare/CreateActivity.java
+++ Dare/src/com/dare/CreateActivity.java
@@ -1,3 +1,4 @@
+
 package com.dare;
 
 import android.app.Activity;
@@ -5,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.facebook.FacebookException;
@@ -14,6 +16,15 @@ import com.facebook.widget.WebDialog;
 import com.facebook.widget.WebDialog.OnCompleteListener;
 
 public class CreateActivity extends Activity {
+
+    private static final String PARAM_MESSAGE_KEY = "message";
+
+    private static final String PARAM_MESSAGE = "You've been challenged!";
+
+    private static final String PARAM_TITLE_KEY = "title";
+
+    private static final String PARAM_CONTENT_KEY = "content";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,43 +35,59 @@ public class CreateActivity extends Activity {
     }
 
     private void sendRequestDialog() {
-        Bundle params = new Bundle();
-        params.putString(
-                "message",
-                "This is a request that should prompt you to get the app and provide a send param");
-        params.putString("send", "a challenge!");
+        Bundle params = getChallengeParams();
+        if (params == null) {
+            Toast.makeText(this, "Please fill in challenge title and details", Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
         WebDialog requestsDialog = (new WebDialog.RequestsDialogBuilder(this,
                 Session.getActiveSession(), params)).setOnCompleteListener(
-                        new OnCompleteListener() {
-                            @Override
-                            public void onComplete(Bundle values,
-                                    FacebookException error) {
-                                if (error != null) {
-                                    if (error instanceof FacebookOperationCanceledException) {
-                                        Toast.makeText(getApplicationContext(),
-                                                "Request cancelled", Toast.LENGTH_SHORT)
-                                                .show();
-                                    } else {
-                                        Toast.makeText(getApplicationContext(),
-                                                "Network Error", Toast.LENGTH_SHORT)
-                                                .show();
-                                    }
-                                } else {
-                                    final String requestId = values
-                                            .getString("request");
-                                    if (requestId != null) {
-                                        Toast.makeText(getApplicationContext(),
-                                                "Request sent", Toast.LENGTH_SHORT)
-                                                .show();
-                                    } else {
-                                        Toast.makeText(getApplicationContext(),
-                                                "Request cancelled", Toast.LENGTH_SHORT)
-                                                .show();
-                                    }
-                                }
+                new OnCompleteListener() {
+                    @Override
+                    public void onComplete(Bundle values,
+                            FacebookException error) {
+                        if (error != null) {
+                            if (error instanceof FacebookOperationCanceledException) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Request cancelled", Toast.LENGTH_SHORT)
+                                        .show();
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Network Error", Toast.LENGTH_SHORT)
+                                        .show();
                             }
-                        }).build();
+                        } else {
+                            final String requestId = values
+                                    .getString("request");
+                            if (requestId != null) {
+                                Toast.makeText(getApplicationContext(),
+                                        "Request sent", Toast.LENGTH_SHORT)
+                                        .show();
+                            } else {
+                                Toast.makeText(getApplicationContext(),
+                                        "Request cancelled", Toast.LENGTH_SHORT)
+                                        .show();
+                            }
+                        }
+                    }
+                }).build();
         requestsDialog.show();
+    }
+
+    private Bundle getChallengeParams() {
+        String challengeTitle = ((EditText) findViewById(R.id.challenge_title)).getText()
+                .toString();
+        String challengeContent = ((EditText) findViewById(R.id.challenge_content)).getText()
+                .toString();
+        if (challengeTitle.isEmpty() || challengeContent.isEmpty()) {
+            return null;
+        }
+        Bundle params = new Bundle();
+        params.putString(PARAM_MESSAGE_KEY, PARAM_MESSAGE);
+        params.putString(PARAM_TITLE_KEY, challengeTitle);
+        params.putString(PARAM_CONTENT_KEY, challengeContent);
+        return params;
     }
 
     private void setupButtons() {
@@ -78,16 +105,6 @@ public class CreateActivity extends Activity {
                 finishCreate();
             }
         });
-        findViewById(R.id.btn_save).setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                saveChallenge();
-            }
-        });
-    }
-
-    private void saveChallenge() {
-        finishCreate();
     }
 
     private void finishCreate() {
